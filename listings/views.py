12fb4b7e5734bd63a1d56179a1dfa98aaf29fb16
bjamyl <alhassanjamil0@gmail.com>
2022-08-10 18:02:25 +0000

    modified views and html pages to receive dynamic content from database

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import bedroom_choices, price_choices, state_choices
+
 
 from .models import Listing
 
@@ -11,13 +13,27 @@ def index(request):
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
     context = {
-        'listings': paged_listings
+        'listings': paged_listings,
+
     }
 
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices
+
+    }
+    return render(request, 'listings/search.html', context)
