831814695572a9d93159f9f79e7b4b35ae81a19a
bjamyl <alhassanjamil0@gmail.com>
2022-08-13 00:37:31 +0000

    added django heroku and postgresql database

--- btre/settings.py
+++ btre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,13 +81,24 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'btredb',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost'
+
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost'
+        'NAME': 'dbi8g15r075c1g',
+        'USER': 'fpsniqrwcsxykl',
+        'PASSWORD': '342ae85d88386326457568f19020db7c494a9abdf627e77d9658ce0d320ca370',
+        'HOST': 'ec2-34-193-44-192.compute-1.amazonaws.com',
+        'PORT': '5432'
 
     }
 }
@@ -131,6 +143,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
